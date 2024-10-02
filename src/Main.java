fd66cbecfb630e75814dd7a78d25d32515932fda
C026-01-07392023 <jerry.gathu23@students.dkut.ac.ke>
2024-10-02 22:16:31 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*CO26-01-0739/2023");
+        System.out.println("#99ACS Building Gilmore Ave. New Mla.. Quezon City * Tel No:123-4567");
+        System.out.println("\tVAT REG. TIN 098-765-4321-0000");
+        System.out.println("\t\tSALES INVOICE\n");
+        System.out.println("Sold to:Jerry Francis Gathu\t\tDate:28/04/2022");
+        System.out.println("Address:private bag\t\t\t\t\tContact Number:0100100100\n");
+        System.out.println("=============================================================================");
+        System.out.println("Qty\t|\tItem Description\t|\tUnit Price\t|\tAmount");
+        System.out.println("=============================================================================");
+        System.out.println("4\t|\tPencil\t\t\t\t|\t20\t\t\t|\t80.00");
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("10\t|\tDuster\t\t\t\t|\t50\t\t\t|\t500.00");
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("9\t|\tPens\t\t\t\t|\t30\t\t\t|\t270.00");
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("10\t|\tcrayon\t\t\t\t|\t80\t\t\t|\t800.00");
+        System.out.println("==============================================================================");
+        System.out.println("                            |Sub Total\t\t|\t1650.00");
+        System.out.println("                            ==============================");
+        System.out.println("                            |VAT(16%)\t\t|\t264.00 ");
+        System.out.println("                            ==============================");
+        System.out.println("                            |Total Amount\t|\t1914.00");
+        System.out.println("                            ==============================");
+    }}
